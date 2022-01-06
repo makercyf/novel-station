2d29a77a48a32f58d1e7f236e2af8c270c0de6a9
makercyf <56283809+makercyf@users.noreply.github.com>
2022-01-06 22:43:05 +0800

    Update syosetu.py

--- syosetu.py
+++ syosetu.py
@@ -15,32 +15,41 @@ if r.status_code == 200:
         code = code[-1]
     ###
     website = BeautifulSoup(r.text, 'html.parser')
-    NovelTitle = website.title.string
+    css = '''<style>
+#novel_ex { margin: 20px auto 50px; line-height: 180%; overflow: hidden; text-align: left; font-size:17px; }
+div.chapter_title {font-size: 110%; padding: 30px 0 0 0; font-weight:bold;}
+div.index_box { font-size: 17px;}
+dl.novel_sublist2 {overflow: hidden; border-bottom:1px solid #ffffff;}
+dl.novel_sublist2 dd{padding: 10px 10px 2px 5px; width: 515px; font-size: 17px; float:left;}</style>\n'''
+    novel_title = website.title.string
+    writer_name = str(website.find_all("div", class_="novel_writername")[0]).split(">")[2].split("<")[0]
+    description = str(website.find(id="novel_ex"))
+    index = str(website.find_all("div", class_="index_box")[0])
     ###
-    ChapterLinkList = []
+    chapter_link = []
     toc = website.find_all("a")
     for chapter in toc:
         link = chapter.get("href")
         match = re.search(code + '/{1,}[0-9]', link)
         if match:
-            ChapterLink = "https://ncode.syosetu.com" + link
-            ChapterLinkList.append(ChapterLink)
-    LargestChapter = len(ChapterLinkList)
+            full_chapter_link = "https://ncode.syosetu.com" + link
+            chapter_link.append(full_chapter_link)
+    largest_chapter = len(chapter_link)
     ###
-    DownloadRange = input("\nDownload range (A for all, or enter a range separtaed by comma): ")
-    DownloadRange = DownloadRange.split(",")
-    if len(DownloadRange) == 2:
-        if DownloadRange[0].isnumeric and DownloadRange[1].isnumeric:
-            first, last = DownloadRange[0].strip(), DownloadRange[1].strip()
+    download_range = input("\nDownload range (A for all, or enter a range separtaed by comma): ")
+    download_range = download_range.split(",")
+    if len(download_range) == 2:
+        if download_range[0].isnumeric and download_range[1].isnumeric:
+            first, last = download_range[0].strip(), download_range[1].strip()
             first, last = int(first), int (last)
-            if first < 0 or last > LargestChapter:
+            if first < 0 or last > largest_chapter:
                 print("Download range is invalid, range set to all chapter")
-                first, last = 1, LargestChapter
+                first, last = 1, largest_chapter
         else:
             print("Download range is invalid, range set to all chapter")
-            first, last = 1, LargestChapter
+            first, last = 1, largest_chapter
     else:
-        first, last = 1, LargestChapter
+        first, last = 1, largest_chapter
     ###
     cn = input("Append chapter number to the file? (Y/n): ")
     if cn == "N" or cn == "n":
@@ -48,11 +57,17 @@ if r.status_code == 200:
     else:
         cn = True
     ###
-    if not os.path.isdir(f'./{NovelTitle}'):
-        os.mkdir(NovelTitle)
-    print(f"\nDownloading {NovelTitle}")
+    if not os.path.isdir(f'./{novel_title}'):
+        os.mkdir(novel_title)
+    with open(f"./{novel_title}/0. index.html", "w", encoding="utf-8") as f:
+        f.write(css)
+        f.write(f'<h1 style="text-align: center;">{novel_title}</h1>\n')
+        f.write(f'<h2 style="text-align: center;">作者：{writer_name}</h2>\n')
+        f.write(description)
+        f.write(index)
+    print(f"\nDownloading {novel_title}")
     for i in range(first, last + 1):
-        website = requests.get(ChapterLinkList[i-1], headers={"user-agent": 'Mozilla/5.0'}).text
+        website = requests.get(chapter_link[i-1], headers={"user-agent": 'Mozilla/5.0'}).text
         website = BeautifulSoup(website, 'html.parser')
         subtitle = website.find_all("p", class_="novel_subtitle")[0].string
         content = website.find_all("p")
@@ -69,12 +84,16 @@ if r.status_code == 200:
             if b in filename:
                 print("Illgeal name detected, progran exit")
                 exit()
-        with open(f"./{NovelTitle}/{filename}.html", "w", encoding="utf-8") as f:
+        with open(f"./{novel_title}/{filename}.html", "w", encoding="utf-8") as f:
             f.write(f'<h1 style="text-align: center;">{subtitle}</h1>\n')
             for line in content:
                 match = re.search('id="L[0-9]{1,}', str(line))
                 if match:
                     f.write(str(line) + "\n")
+        with open(f"./{novel_title}/0. index.html", "r+", encoding="utf-8") as f:
+            new = f.read().replace(f'"/{code}/{i}/"', f'"./{filename}.html" target="_blank"')
+            f.seek(0)
+            f.write(new)
         print(f"Progress: {i}/{last}", end="\r")
     print("\nDone")
 else:
